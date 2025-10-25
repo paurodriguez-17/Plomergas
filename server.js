6c1040570c3d5d366a7b480a12c2d039a4222744
Pau Rodriguez <fabricaaranda@gmail.com>
2025-10-25 13:11:11 -0300

    terminado

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const path = require('path');
 const fs = require('fs');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3070;
 
 // Middlewares
 app.use(cors());
